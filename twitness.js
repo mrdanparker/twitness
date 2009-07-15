ca37ba86a8e56666c8a640b1436eade8b30f30a1
Richard Paul <richard.a.paul@gmail.com>
2009-07-15 22:40:29 +0100

    Now shows latest 10 tweets with 'hundredpushups' in them

--- twitness.js
+++ twitness.js
@@ -1,1 +1,13 @@
-// Javascript will go here
+$(function() {
+  var twitterSearch = "http://search.twitter.com/search.json?rpp=10&callback=?&q=";
+  $.getJSON(twitterSearch + "hundredpushups", function(data) {
+    for (var i in data.results) {
+      var tweet = data.results[i];
+      var p = $("<p/>"); 
+      var user = $("<span/>").text(tweet.from_user).addClass("username");
+      var text = $("<span/>").text(tweet.text);
+      var p = $("<p/>").append(user).append(text)
+      $('body').append(p);
+    }
+  });
+});
