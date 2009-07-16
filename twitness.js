443094b88419325844b8cec0acb09309c3de4ce7
danparker <mr.danny.parker@gmail.com>
2009-07-16 18:55:18 +0100

    didnt add modified files in last commit - included now

--- twitness.js
+++ twitness.js
@@ -1,13 +1,12 @@
 $(function() {
-  var twitterSearch = "http://search.twitter.com/search.json?rpp=10&callback=?&q=";
-  $.getJSON(twitterSearch + "hundredpushups", function(data) {
-    for (var i in data.results) {
-      var tweet = data.results[i];
-      var p = $("<p/>"); 
-      var user = $("<span/>").text(tweet.from_user).addClass("username");
-      var text = $("<span/>").text(tweet.text);
-      var p = $("<p/>").append(user).append(text)
-      $('body').append(p);
-    }
-  });
+	$.twitterSearch({searchText:"hundredpushups"}, function(results) {
+		for (var i in results) {
+			var tweet = results[i];
+			var p = $("<p/>"); 
+			var user = $("<span/>").text(tweet.from_user).addClass("username");
+			var text = $("<span/>").text(tweet.text);
+			var p = $("<p/>").append(user).append(text)
+			$('body').append(p);
+		}
+        });
 });
