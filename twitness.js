df5112a2f9d5beb15efc627dc23fbb5381636c50
Richard Paul <richard.a.paul@gmail.com>
2009-07-19 17:47:28 +0100

    Added avatar's for tweeps, encoded query string as Dan suggested, centred the page

--- twitness.js
+++ twitness.js
@@ -1,12 +1,16 @@
 $(function() {
-	$.twitterSearch({searchText:"hundredpushups"}, function(results) {
+	$.twitterSearch({searchText:"#hundredpushups"}, function(results) {
 		for (var i in results) {
 			var tweet = results[i];
 			var p = $("<p/>"); 
 			var user = $("<span/>").text(tweet.from_user).addClass("username");
 			var text = $("<span/>").text(tweet.text);
-			var p = $("<p/>").append(user).append(text)
-			$('body').append(p);
+		      	var link = $('<a/>').attr({href:"http://twitter.com/" + tweet.from_user});
+			var img = $('<img/>').attr({src:tweet.profile_image_url, alt:"avatar"});
+  			$(link).append(img);
+			var p = $("<p/>").append(link).append(user).append(text).addClass('tweet');
+			$('#recent').append(p);
 		}
         });
 });
+
