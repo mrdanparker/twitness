3f2cf879d0d2e2598982a7344e3cf6266844dec6
Richard Paul <richard.a.paul@gmail.com>
2009-07-19 17:45:29 +0100

    Added avatar's for tweeps, encoded query string as Dan suggested, centred the page

--- jquery.twitter.js
+++ jquery.twitter.js
@@ -6,29 +6,29 @@
 				searchText: invalidSearch ? "mrdanparker OR rapaul OR alexcuesta" : options.searchText
 				}); // notice the easter egg!
 				
-            var url = "http://search.twitter.com/search.json?callback=?";           
+            var url = 'http://search.twitter.com/search.json?callback=?';
 			
             // limit results if required
             if(options.limit){
-                url += "&rpp=" + options.limit
+                url += "&rpp=" + options.limit;
             }
 
             // build q=...
             url += "&q=";
             var plus = "";
             if(options.searchText){
-                url += plus + options.searchText;
+                url += plus + encodeURIComponent(options.searchText);
                 plus = "+";
             }
             if(options.from){
-                url += plus + "from:" + options.from;
+                url += plus + "from:" + encodeURIComponent(options.from);
                 plus = "+";
             }
             if(options.to){
-                url += plus + "to:" + options.to;
+                url += plus + "to:" + encodeURIComponent(options.to);
                 plus = "+";
             }
-			
+
             // request the JSON and invoke func to deal with the result
             $.getJSON(url, function(data){
                 if(typeof func == 'function'){
