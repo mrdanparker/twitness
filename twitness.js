51d84193c9c54f94bf4ff2c8a4bdc446d7dd84d6
Dan Parker <mr.danny.parker@gmail.com>
2009-07-22 22:59:35 +0100

    workout sets, pass and fail highlighting plus updating window.location.hash to the searched for user

--- twitness.js
+++ twitness.js
@@ -1,6 +1,32 @@
-$(function() {
-	showTweets();
+//Define some global vars
+var setsHighlight = {
+	regex : new RegExp("(\\d{1,3}([-,:\\\\/;|\\+]) ?\\d{1,3}(\\2 ?\\d{1,3}){3,7})", "g"),
+	replacement : "<span class=\"sets\">$1</span>"
+};
+var failHighlight = {
+	regex : new RegExp("(#?fail(ed)?(ure)?)","gi"),
+	replacement : "<span class=\"fail\">$1</span>"
+};
+var passHighlight = {
+	regex : new RegExp("(#?pass(ed)?)","gi"),
+	replacement : "<span class=\"pass\">$1</span>"
+};
+var allTextReplacements = [
+	setsHighlight,
+	failHighlight,
+	passHighlight
+];
 
+$(function(){
+
+	// Get the username hash from the url to pass into showTweets
+	var hash = "";
+	if(window.location.hash){
+		hash = window.location.hash.replace("#","");	
+	}
+	showTweets(hash);
+
+	// Make <enter> in username input trigger showTweets
 	$("#username").keyup(function(e){
 		if(e.keyCode == 13) {
 			showTweets($("#username").val());
@@ -28,27 +54,43 @@ $(function() {
 	
 });
 
-function showTweets(username)
-{
-	$.twitterSearch({searchText:"hundredpushups", from: username}, function(results) {
+function showTweets(username){
+	$.twitterSearch({searchText: "hundredpushups", from: username}, function(results){
 		// clear previous contents
-		$('#recent').slideUp(400, function()
-		{
+		$('#recent').slideUp(400, function(){
 			$('#recent').empty();
 			// append new contents
 			for (var i in results) {
 				var tweet = results[i];
 				var p = $("<p/>"); 
 				var user = $("<span/>").text(tweet.from_user).addClass("username");
-				var text = $("<span/>").text(tweet.text);
+				var text = $("<span/>").text(tweet.text).addClass("tweetText");
 			      	var link = $('<a/>').attr({href:"http://twitter.com/" + tweet.from_user});
 				var img = $('<img/>').attr({src:tweet.profile_image_url, alt:"avatar"});
 	  			$(link).append(img);
 				var p = $("<p/>").append(link).append(user).append(text).addClass('tweet');
 				$('#recent').append(p);
 			}
+			applyTextReplacements(".tweetText");
+			updateAddressBarHash(username);
 			$('#recent').slideDown(400);
 		});
         });
+}
+
+// Updates the address bar hash with the given value
+function updateAddressBarHash(value){
+	if(value){
+		window.location.hash = value;
+	}
+}
+
+// Apply allTextReplacements to elements matching the selector
+function applyTextReplacements(selector) {
+	$(selector).each(function() {
+		for(var i in allTextReplacements) {
+			this.innerHTML = this.innerHTML.replace(allTextReplacements[i].regex, allTextReplacements[i].replacement);
+		}
+	});
 }
 
