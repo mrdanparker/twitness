0cef454ca5eb76ea0776fc7967f9aca08af7d547
Richard Paul <richard.a.paul@gmail.com>
2009-09-19 16:17:29 +0100

    Added limit of 30 tweets to the home page

--- war/WEB-INF/groovy/api/public.groovy
+++ war/WEB-INF/groovy/api/public.groovy
@@ -1,15 +1,16 @@
 import com.google.appengine.api.datastore.*
 import net.sf.json.JSONObject
 import net.sf.json.JSONArray
+import static com.google.appengine.api.datastore.FetchOptions.Builder.*;
 
 switch (request.method) {
 	case "GET" :
         Query query = new Query("tweet")
         .addSort("created_at", Query.SortDirection.DESCENDING)
-        Iterator<Entity> tweets = datastoreService.prepare(query).asIterator()
+        List<Entity> tweets = datastoreService.prepare(query).asList(withLimit(30)) // Only show the latest 30 tweets on homepage
         
         JSONArray jsonArray = new JSONArray()
-        for(Entity tweet : tweets ) {
+        for(Entity tweet : tweets) {
         	JSONObject tweetJson = new JSONObject()
         	tweetJson.put("text", tweet.text)
         	tweetJson.put("to_user_id", tweet.to_user_id)
